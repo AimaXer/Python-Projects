87223d90ea4482409ed0581c9d2e4891bf086df3
Maciej Chmielewski <maciej.jakub.chmielewski@gmail.com>
2019-04-13 19:27:44 +0200

    MD: Działający update

--- Django/site_1/main/views.py
+++ Django/site_1/main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os.path
+
+def getHtml():
+    file_path = os.path.dirname(os.path.realpath(__file__)) + "\\html_v1.txt"
+    f = open(file_path, 'r', encoding = 'UTF-8') 
+    html_v2_string = f.read()
+    f.close()
+    return html_v2_string
 
 def v1(response):
     return HttpResponse("<h1>Samolot</h1>")
 
-with open('html_v1.html', 'r') as f: 
-    html_v2_string = f.read()
-
 def v2(response):
-    return HttpResponse(html_v2_string)
+    return HttpResponse(getHtml())
