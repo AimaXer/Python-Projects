0f7b28bc528b8dc187cb172730544f49049eef3f
Maciej Chmielewski <maciej.jakub.chmielewski@gmail.com>
2019-04-13 20:43:24 +0200

    MC: Dodanie kozystania z DB na podstawie przykładu - WORKS

--- Django/site_1/main/views.py
+++ Django/site_1/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os.path
+from .models import ToDoList, Item
 
 def getHtml():
     file_path = os.path.dirname(os.path.realpath(__file__)) + "\\html_v1.txt"
@@ -9,8 +10,10 @@ def getHtml():
     f.close()
     return html_v2_string
 
-def v1(response):
-    return HttpResponse("<h1>Samolot</h1>")
+def v1(response, name):
+    ls = ToDoList.objects.get(name=name)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(items.text)))
 
 def v2(response):
     return HttpResponse(getHtml())
