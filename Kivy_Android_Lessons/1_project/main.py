e4df13819363b403c09948441081c6c8c0f7add1
AimaXer <maciej.jakub.chmielewski@gmail.com>
2019-05-09 15:08:09 +0200

    MC: Test commita przez pycharm

--- Kivy_Android_Lessons/1_project/main.py
+++ Kivy_Android_Lessons/1_project/main.py
@@ -6,14 +6,24 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.graphics import Line
+from kivy.lang import Builder
+
+presentation = Builder.load_file('widgets2.kv')
 
 class Widgets(Widget):
     def on_touch_down(self, touch):
         print(touch)
+        with self.canvas:
+            touch.ud["line"] = Line(points=(touch.x, touch.y))
+
     def on_touch_move(self, touch):
         print(touch)
+        touch.ud["line"].points += (touch.x, touch.y)
+
     def on_touch_up(self, touch):
-        print("RELEASED!",touch)
+        print("RELEASED!", touch)
+
 
 class LoginScreen(GridLayout):
 
@@ -28,11 +38,11 @@ class LoginScreen(GridLayout):
         self.add_widget(self.password)
         self.add_widget(Widgets())
 
-class widgets2(App):
+class main(App):
 
     def build(self):
-        return Widgets()
+        return LoginScreen()
 
 
 if __name__ == '__main__':
-    widgets2().run()
+    main().run()
