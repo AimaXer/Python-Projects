7b56d9fb729f39c654e9144636fbb19a8d67b52b
AimaXer <maciej.jakub.chmielewski@gmail.com>
2019-05-16 16:14:21 +0200

    MC: Usprawnienie Django, przeniesienie plików strony do /templates, nadal do poprawy

--- Django/site_1/main/urls.py
+++ Django/site_1/main/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:name>1", views.v1, name="v1"),
-    path("v2/", views.v2, name="v2"),
+    path("", views.home)
 ]
