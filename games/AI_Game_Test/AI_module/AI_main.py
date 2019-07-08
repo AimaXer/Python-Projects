2ecceb142b183f70e95c1aeb5a0e8fa0d86cbe99
AimaXer <maciej.jakub.chmielewski@gmail.com>
2019-07-08 09:23:43 +0200

    MC: update

---
+++ games/AI_Game_Test/AI_module/AI_main.py
@@ -0,0 +1,9 @@
+
+class main():
+
+    @staticmethod
+    def main():
+        pass
+
+if __name__ == '__main__':
+    main.main()
--- games/AI_Game_Test/main.py
+++ games/AI_Game_Test/main.py
@@ -1,10 +1,11 @@
 
 import pygame
 import random
+from AI_module.AI_main import main as AI
 
 #GLOBAL VARS
 screen_width = 1000
-screen_height = 800
+screen_height = 600
 rects_pos = [[100,100],[200,100],[300,100],[400,100],[500,100],[600,100],[700,100],[800,100],[100,300],[300,300]]
 
 rect_size = [50,50]
@@ -21,54 +22,61 @@ class Rect():
     def rectDraw(screen, obj):
         pygame.draw.rect(screen,(0,128,254), obj)
 
+class GUI():
 
-def check_outerlines_collision(pos):
-    if pos[0] < 0:
-        pos[0] = 0
+    @staticmethod
+    def check_outerlines_collision(pos):
+        if pos[0] < 0:
+            pos[0] = 0
 
-    if pos[0] > screen_width - rect_size[0]:
-        pos[0] = screen_width - rect_size[0] - 1
+        if pos[0] > screen_width - rect_size[0]:
+            pos[0] = screen_width - rect_size[0] - 1
 
-    if pos[1] < 0:
-        pos[1] = 0
+        if pos[1] < 0:
+            pos[1] = 0
 
-    if pos[1] > screen_height - rect_size[1]:
-        pos[1] = screen_height - rect_size[1] - 1
+        if pos[1] > screen_height - rect_size[1]:
+            pos[1] = screen_height - rect_size[1] - 1
 
-    return pos
+        return pos
 
-def main_function():
+    @staticmethod
+    def main_function():
 
-     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return True
+         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return True
 
-        print(rects_pos[1][0])
-        print(rects_pos[1][1])
+            iterator = 0
 
-        for i in rects_pos:
-            rectangle = Rect.rectangle(i)
-            Rect.rectDraw(screen, rectangle)
-            
-            for x in range(0, 10):
-                keys = pygame.key.get_pressed()
-                if keys[pygame.K_LEFT]:
-                    rects_pos[x][0] += -1
-                if keys[pygame.K_RIGHT]:
-                    rects_pos[x][0] += 1
-                if keys[pygame.K_UP]:
-                    rects_pos[x][1] += -1
-                if keys[pygame.K_DOWN]:
-                    rects_pos[x][1] += 1
+            for i in rects_pos:
+                rectangle = Rect.rectangle(i)
+                Rect.rectDraw(screen, rectangle)
 
-                # rects_pos[x][0] += random.randint(-1, 1)
-                # rects_pos[x][1] += random.randint(-1, 1)
+                for x in range(0, 10):
+                    keys = pygame.key.get_pressed()
+                    if keys[pygame.K_LEFT]:
+                        rects_pos[x][0] += -1
+                    if keys[pygame.K_RIGHT]:
+                        rects_pos[x][0] += 1
+                    if keys[pygame.K_UP]:
+                        rects_pos[x][1] += -1
+                    if keys[pygame.K_DOWN]:
+                        rects_pos[x][1] += 1
 
-            i = check_outerlines_collision(i)
+                    if iterator % 10 == 0:
+                        ran_move_x = random.randint(-2, 2)
+                        ran_move_y = random.randint(-2, 2)
 
-        pygame.display.flip()
-        pygame.time.Clock().tick(60)
-        screen.fill((0,0,0))
+                    rects_pos[x][0] += ran_move_x
+                    rects_pos[x][1] += ran_move_y
 
-main_function()
+                i = GUI.check_outerlines_collision(i)
+                iterator += 1
+            pygame.display.flip()
+            pygame.time.Clock().tick(60)
+            screen.fill((0,0,0))
+
+if __name__ == '__main__':
+    GUI.main_function()
