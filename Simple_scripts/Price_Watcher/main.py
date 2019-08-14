5a13dc8f73b252d89c8cd221d8c5e75bf4b56134
AimaXer <maciej.jakub.chmielewski@gmail.com>
2019-08-14 13:20:09 +0200

    MC: Fast Update, Work Laptop

--- Simple_scripts/Price_Watcher/main.py
+++ Simple_scripts/Price_Watcher/main.py
@@ -25,23 +25,25 @@ def main():
         for item in data:
             response = requests.get(item['link'], headers=hdrs)
             page_content = bs(response.content, "html.parser")
-
-            price = int(float(re.sub(',','.',re.sub('[A-Za-złńąść ]','', get_item(page_content, '_wtiln _bdn9q _9a071_2MEB_')))))
-            time_left = unicodedata.normalize("NFKD",get_item(page_content, '_9a071_9F8-G')).encode('ascii', 'ignore')
-            title = unicodedata.normalize("NFKD",get_item(page_content, '_1h7wt _15mod')).encode('ascii', 'ignore')
-
+            try:
+                price = int(float(re.sub(',','.',re.sub('[A-Za-złńąść ]','', get_item(page_content, '_wtiln _bdn9q _9a071_2MEB_')))))
+                time_left = unicodedata.normalize("NFKD",get_item(page_content, '_9a071_9F8-G')).encode('ascii', 'ignore')
+                title = unicodedata.normalize("NFKD",get_item(page_content, '_1h7wt _15mod')).encode('ascii', 'ignore')
+            except ValueError as e:
+                print('Value Error Line 31-33 ('+str(e)+'), continue')
+                continue
             try:
                 time_left_int_min = int(time_left[3:5].decode("utf-8"))
                 time_left_int_hr = int(time_left[0:2].decode("utf-8"))
             except ValueError as e:
-                print('Value Error Line 30-34 ('+str(e)+')')
+                print('Value Error Line 30-34 ('+str(e)+'), continue')
                 continue
 
             try:
                 if int(item['price']) >= price and time_left_int_min <= time_before_end_min and time_left_int_hr <= time_before_end_hr:
                     send_mail(item['link'], item['price'], time_left, title)
             except ValueError as e:
-                print('Value Error Line 40-41 ('+str(e)+')')
+                print('Value Error Line 40-41 ('+str(e)+'), continue')
                 continue
             time.sleep(300)
 
@@ -57,7 +59,7 @@ def send_mail(url, wanted_price, time_left, name):
     subject = str(name.decode("utf-8"))+' | Allegro PW v'+version
     body = 'Aukcja konczy sie ponizej ceny '+str(wanted_price)+'zl\nDo konca aukcji pozostalo mniej niz 10 min !\n('+str(time_left.decode("utf-8"))+')\n\nLink do aukcji: '+str(url)
 
-    msg = f"Subject: {subject}\n\n{body}"
+    msg = "Subject: "+subject+"\n\n"+body
 
     server.sendmail(
         'maciej.jakub.chmielewski@gmail.com',
@@ -67,6 +69,3 @@ def send_mail(url, wanted_price, time_left, name):
 
 if __name__ == '__main__':
    main()
-
-
-
