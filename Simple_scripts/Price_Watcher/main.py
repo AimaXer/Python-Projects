955259a46a8538a95003ae3b67be3b983f30da90
AimaXer <maciej.jakub.chmielewski@gmail.com>
2019-08-14 10:49:14 +0200

    MC: Fast Update, Work Laptop

--- Simple_scripts/Price_Watcher/main.py
+++ Simple_scripts/Price_Watcher/main.py
@@ -2,40 +2,49 @@ import requests, re
 from bs4 import BeautifulSoup as bs
 import smtplib
 import unicodedata
-import time
+import time, json
 
-version = 1.0
+version = "1.0.0"
+hdrs = {
+    'Content-type': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+time_before_end_min = 30
+time_before_end_hr = 0
 
 def get_item(content, id):
     return content.find("div", {"class":id}).text
 
+def get_data():
+    with open("data.txt") as file:
+        data = json.load(file)['data']
+    return data
+
 def main():
-    hdrs = {
-        'Content-type': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
-    url = "https://allegro.pl/oferta/macbook-pro-15-i7-16gb-240-ssd-bcm-8381191094"
-    response = requests.get(url, headers=hdrs)
+    while True:
+        data = get_data()
 
-    wanted_price = 10000
+        for item in data:
+            response = requests.get(item['link'], headers=hdrs)
+            page_content = bs(response.content, "html.parser")
 
-    page_content = bs(response.content, "html.parser")
+            price = int(float(re.sub(',','.',re.sub('[A-Za-złńąść ]','', get_item(page_content, '_wtiln _bdn9q _9a071_2MEB_')))))
+            time_left = unicodedata.normalize("NFKD",get_item(page_content, '_9a071_9F8-G')).encode('ascii', 'ignore')
+            title = unicodedata.normalize("NFKD",get_item(page_content, '_1h7wt _15mod')).encode('ascii', 'ignore')
 
-    price = int(float(re.sub(',','.',re.sub('[A-Za-złńąść ]','', get_item(page_content, '_wtiln _bdn9q _9a071_2MEB_')))))
-    time_left = unicodedata.normalize("NFKD",get_item(page_content, '_9a071_9F8-G')).encode('ascii', 'ignore')
-    title = unicodedata.normalize("NFKD",get_item(page_content, '_1h7wt _15mod')).encode('ascii', 'ignore')
+            try:
+                time_left_int_min = int(time_left[3:5].decode("utf-8"))
+                time_left_int_hr = int(time_left[0:2].decode("utf-8"))
+            except ValueError as e:
+                print('Value Error Line 30-34 ('+str(e)+')')
+                continue
 
-    time_left_int15 = 0
-    time_left_int00 = '01'
+            try:
+                if int(item['price']) >= price and time_left_int_min <= time_before_end_min and time_left_int_hr <= time_before_end_hr:
+                    send_mail(item['link'], item['price'], time_left, title)
+            except ValueError as e:
+                print('Value Error Line 40-41 ('+str(e)+')')
+                continue
+            time.sleep(300)
 
-    try:
-        time_left_int15 = int(time_left[3:5].decode("utf-8"))
-        time_left_int00 = str(time_left[0:2].decode("utf-8"))
-        print(time_left_int00)
-        print(time_left_int15)
-    except ValueError:
-        print('Time is not in intiger type')
-
-    if wanted_price > price and time_left_int15 < 150 and time_left_int00 == '18':
-        send_mail(url, wanted_price, time_left, title)
 
 def send_mail(url, wanted_price, time_left, name):
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -45,7 +54,7 @@ def send_mail(url, wanted_price, time_left, name):
 
     server.login('maciej.jakub.chmielewski@gmail.com', 'ohgxepsdirjpemfe')
 
-    subject = str(name.decode("utf-8"))+' | Allegro PW v'+str(version)
+    subject = str(name.decode("utf-8"))+' | Allegro PW v'+version
     body = 'Aukcja konczy sie ponizej ceny '+str(wanted_price)+'zl\nDo konca aukcji pozostalo mniej niz 10 min !\n('+str(time_left.decode("utf-8"))+')\n\nLink do aukcji: '+str(url)
 
     msg = f"Subject: {subject}\n\n{body}"
